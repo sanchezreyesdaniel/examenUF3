90649a5fad7bbe8f3f468dbeb920326c6353d000
sanchezreyesdaniel <sanchezreyesdaniel@fpllefia.com>
2024-03-11 17:39:51 +0100

    2

--- src/componentes/pedido.js
+++ src/componentes/pedido.js
@@ -8,21 +8,25 @@ export const pedido = {
       
       <div class="col-6">
         <h3>Grupo</h3>
+        <form id="form">
         <label for="nombreGrupo" class="label-control">Nombre del grupo:</label>
-        <input type="text" class="form-control mt-2" placeholder ="Borrachos de DAW2">
-        <label for="numeroMesa" class="label-control">Mesa numero</label>
-        <input type="number" class="form-control mt-2" placeholder ="0">
+        <input id="grupo" type="text" class="form-control" required minlength="4" maxlength="10" mt-2" placeholder ="Borrachos de DAW2">
+        <label for="numeroMesa" class="label-control class="form-control" >Mesa numero</label>
+        <div  class="invalid-feedback" class="form-control invalid-feedback" >El número de mesa debe estar entre 1 y 15.</div>
+        <input id="mesa" type="number" class="form-control mt-2" required min="0" max="15" required min="1" placeholder ="0">
       
         <h3 class="mt-5">Haz tu pedido</h3>
         <div class="d-flex gap-3 ">
-          <select name="cervezas" id="cervezas" class="form-control">
-            <option value="">Selecciona qué birra quieres</option>
-            <option value="">Estrella Galicia</option>
+          <select name="cervezas" id="cervezas" class="form-control " required >
+            <option  value="">Selecciona qué birra quieres</option>
+            <option  value="">Estrella Galicia</option>
           </select>
-        
-          <input type="number" value="0" class="form-control">
+          <div class="invalid-feedback">Selecciona una cerveza.</div>
+          <input id="cantidad" type="number" value="0" class="form-control" required min="1">
+          <div class="invalid-feedback">La cantidad de cervezas debe ser mayor que 0.</div>
         </div>
-        <button class="btn btn-success mt-4 w-100">¡Enviar pedido!</button>
+        <button id="enviar" class="btn btn-success mt-4 w-100">¡Enviar pedido!</button>
+        </form>
       </div>
       <div id="carta" class="col-6 border ">
         <div class="p-3 d-flex">
@@ -42,13 +46,25 @@ export const pedido = {
 
   </div>`,
   script:()=>{
+
+  
+
+
+
+
+
+
     let optionCerveza = '<option value="">Selecciona qué birra quieres</option>'
     bd.map((item) =>{
-        optionCerveza += `<option value="${item.id}">${item.nombre}</option>`
+        optionCerveza += `<option  value="${item.id}">${item.nombre}</option>`
       
     })
 
     document.querySelector('#cervezas').innerHTML = optionCerveza
+    // document.querySelector('#enviar').addEventListener('click',()=>{
+        
+    // })
+    
 
     document.querySelector('#cervezas').addEventListener('change',()=>{
         let cervezaValue = document.querySelector('#cervezas').value
@@ -73,9 +89,15 @@ export const pedido = {
         });
         
     })
+    
+    
+}
+    
 
   
 
     }
+    
   
-}
+
+
--- src/componentes/tablaPedidos.js
+++ src/componentes/tablaPedidos.js
@@ -1,71 +1,84 @@
-import { comanda } from "./comanda";
+import { comanda } from "../bd/comanda";
+import { pedido } from "./pedido";
 
 export const tablaPedido = {
-    template: `<div id="tablaPedidos" class="container mt-5 mb-5 p-5 border shadow-lg ">
-    <div class="row">
-      <h1 class="text-center mb-5 ">----- Vista camareros -----</h1>
-    <h3>Pedidos</h3>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Grupo</th>
-          <th>Mesa</th>
-          <th>Cerveza</th>
-          <th>Cantidad</th>
-          <th>Estado</th>
-        </tr>        
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Borrachos de DAW2</td>
-          <td>1</td>
-          <td>Estrella Galicia</td>
-          <td>3</td>
-          <td>
-            <div class="d-flex gap-2">
-              <button class="btn btn-outline-warning w-100 btn-sm">Pedido pendiente...</button>
-              <button class="btn btn-outline-danger w-100 btn-sm"> 🗑 Borrar pedido</button>
-            </div>
-            
-          </td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Cabezones contentos</td>
-          <td>1</td>
-          <td>Estrella DAM</td>
-          <td>2</td>
-          <td>
-            <div class="d-flex gap-2">
-              <button class="btn btn-outline-success w-100 btn-sm">¡Pedido servido!</button>
-              <button class="btn btn-outline-danger w-100 btn-sm"> 🗑 Borrar pedido</button>
-            </div>       
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    </div>
-    
-  </div>`,
+    template: `
+    <div id="tablaPedidos" class="container mt-5 mb-5 p-5 border shadow-lg ">
+        <div class="row">
+            <h1 class="text-center mb-5 ">----- Vista camareros -----</h1>
+            <h3>Pedidos</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Grupo</th>
+                        <th>Mesa</th>
+                        <th>Cerveza</th>
+                        <th>Cantidad</th>
+                        <th>Estado</th>
+                    </tr>
+                </thead>
+                <tbody id="pedidos"></tbody>
+            </table>
+        </div>
+    </div>`,
     script: () => {
+        document.querySelector('#enviar').addEventListener('click', (event) => {
+            event.preventDefault()
+            const form = document.getElementById('form');
+            if (!form.checkValidity()) {
+                form.classList.add('was-validated');
+                return; 
+            }
+            const nuevaComanda = {
+                numeroMesa: document.querySelector('#mesa').value,
+                grupo: document.querySelector('#grupo').value,
+                cerveza: document.querySelector('#cervezas').options[document.querySelector('#cervezas').selectedIndex].text,
+                cantidad: document.querySelector('#cantidad').value,
+                estado: 'pendiente'
+            };
+           
 
-        
+            comanda.push(nuevaComanda);
+            pintarTabla(comanda);
+        });
 
-    
-        function agregarComanda(numeroMesa, grupo, cerveza, cantidad) {
-                const nuevaComanda = {
-                    numeroMesa: numeroMesa,
-                    grupo: grupo,
-                    cerveza: cerveza,
-                    cantidad: cantidad,
-                    estado: 'pendiente'
-                }
+        function pintarTabla(pedidos) {
+            let html = '';
+            pedidos.forEach((pedido, index) => {
+                html += `
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${pedido.grupo}</td>
+                    <td>${pedido.numeroMesa}</td>
+                    <td>${pedido.cerveza}</td>
+                    <td>${pedido.cantidad}</td>
+                    <td>
+                        <div class="d-flex gap-2">
+                            <button class="btn btn-outline-success w-100 btn-sm">¡Pedido servido!</button>
+                            <button  class=" eliminar btn btn-outline-danger w-100 btn-sm" data-index="${index}"> 🗑 Borrar pedido</button>
+                        </div>
+                    </td>
+                </tr>`;
+            });
 
-        comanda.push(nuevaComanda);
+            document.querySelector('#pedidos').innerHTML = html;
+        }
+        function Eliminar(array, index) {
+            array.splice(index, 1);
+            pintarTabla(array);
         }
 
+        document.querySelector('body').addEventListener('click', (e) => {
+            
+        
+            if (e.target.classList.contains('eliminar')) {
+                let index = e.target.dataset.index;
+                Eliminar(comanda, index);
+            }
+        });
 
+
+        
     }
 };
